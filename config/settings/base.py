d13a9a989840cf5486a58d405a77d3e1fa6f4bd5
V <v.donchev@serveza.net>
2025-10-20 21:35:27 +0000

    feat: enable DRF browsable API and add root redirect

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ],
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,9 @@
 from __future__ import annotations
 
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/api/", permanent=False)),
     path("api/", include("apps.profiles.urls")),
 ]
